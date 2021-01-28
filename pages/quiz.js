d497cd6b1556b4432513e08de0d379f9968ab1ff
guijun13 <guilhermejun13.grillo@gmail.com>
2021-01-28 00:33:48 -0300

    feat(quiz page): create loading and quiz questions/answers states

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
@@ -20,38 +21,93 @@ function LoadingScreen() {
   );
 }
 
+function QuestionWidget({ question, totalQuestions, questionIndex }) {
+  const questionId = `question__${questionIndex}`;
+  return (
+    <Widget>
+      <Widget.Header>
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+      <img
+        src={question.image}
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+      />
+      <Widget.Content>
+
+        <h2>{question.title}</h2>
+        <p>{question.description}</p>
+
+        <form>
+
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative__${alternativeIndex}`;
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+              >
+                <input
+                  // style={{ display: 'none' }}
+                  id={alternative}
+                  type="radio"
+                  name={questionId}
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          <Button>
+            Confirmar
+          </Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+
+  );
+}
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LAODING',
+  RESULT: 'RESULT',
+};
+
 export default function QuizPage() {
+  const [screenState, setScreenState] = useState(screenStates.LOADING);
+  const questionIndex = 0;
+  const totalQuestions = db.questions.length;
+  const question = db.questions[questionIndex];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  }, []);
+
   return (
     <QuizBackground>
       <QuizContainer>
-        <Widget>
-          <Widget.Header>
-            <h3>
-              Pergunta 1 de
-              {` ${db.questions.length}`}
-            </h3>
-          </Widget.Header>
-          <img
-            src="https://placehold.it/400x400"
-            alt="Descrição"
-            style={{
-              width: '100%',
-              height: '150px',
-              objectFit: 'cover',
-            }}
-          />
-          <Widget.Content>
 
-            <h2>Titulo</h2>
-            <p>Descrição</p>
+        {screenState === screenStates.QUIZ
+         && (
+         <QuestionWidget
+           question={question}
+           totalQuestions={totalQuestions}
+           questionIndex={questionIndex}
+         />
+         )}
 
-            <Button>
-              Confirmar
-            </Button>
-          </Widget.Content>
-        </Widget>
+        {screenState === screenStates.LOADING && <LoadingScreen />}
 
-        <LoadingScreen />
+        {screenState === screenStates.RESULT && <div>Voce acertou x questões, parabéns!</div>}
 
       </QuizContainer>
     </QuizBackground>
